16cbb8c0cd34b3026a5d3496e3fe5b4514ba0af6
Peyman Hassani <hassani.peyman627@gmail.com>
2021-02-05 18:02:29 +0330

    string comparison examples

---
+++ string_comparision/example.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+STR1="hello buddy"
+STR2="Hello buddy"
+
+if [ "$STR1" = "$STR2" ]; then
+    echo equal
+else
+    echo not equal
+fi
+
+# bash specific not widely used
+if [[ $STR1 = $STR2 ]]; then
+    echo equal
+else
+    echo not equal
+fi
